ee345d8aa24729458aed950f0154149fa5813987
Sanjogsharma <ss3ev@virginia.edu>
2018-09-25 01:11:58 -0400

    Create Game.java
    game method for Hangman

---
+++ Game.java
@@ -0,0 +1,17 @@
+class Game {
+ private String answer;
+ private String hits; 
+ private String misses; 
+  
+  
+ public Game (String answer) {
+   this.answer = answer; 
+   hits - ""; 
+   misses = ""; 
+ }
+  
+ public boolean applyGuess
+
+   
+ }
+}
